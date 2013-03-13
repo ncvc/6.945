70682720bdfa3353bc5d15aa966ba37b548faaf7
Nathan V-C <ncvc@mit.edu>
2013-03-13 06:25:49 -0400

    pset5 Done

--- pset5/rules.scm
+++ pset5/rules.scm
@@ -111,3 +111,80 @@
  (algebra-2 '(+ (* 3 (+ x 1)) -3))
  ;Value: (* 3 x)
 |#
+
+
+;; 5.7
+
+(define algebra-3
+  (rule-simplifier
+   (list
+
+    ;; Sums
+
+    (rule `(+ (? a)) a)
+
+    (rule `(+ (?? a) (+ (?? b)) (?? c))
+    `(+ ,@a ,@b ,@c))
+
+    (rule `(+ (?? a) (? y) (? x) (?? b))
+    (and (expr<? x y)
+         `(+ ,@a ,x ,y ,@b)))
+    
+
+    ;; Products
+
+    (rule `(* (? a)) a)
+
+    (rule `(* (?? a) (* (?? b)) (?? c))
+    `(* ,@a ,@b ,@c))
+
+    (rule `(* (?? a) (? y) (? x) (?? b))
+    (and (expr<? x y)
+         `(* ,@a ,x ,y ,@b)))
+
+
+    ;; Numerical simplifications below
+
+    (rule `(+ 0 (?? x)) `(+ ,@x))
+
+    (rule `(+ (? x ,number?) (? y ,number?) (?? z))
+    `(+ ,(+ x y) ,@z))
+
+
+    (rule `(* 0 (?? x)) 0)
+     
+    (rule `(* 1 (?? x)) `(* ,@x))
+
+    (rule `(* (? x ,number?) (? y ,number?) (?? z))
+      `(* ,(* x y) ,@z))
+
+
+    ;; Distributive law
+
+    (rule `(* (?? a) (+ (?? b)) (?? c))
+    `(+ ,@(map (lambda (x) `(* ,@a ,x ,@c)) b)))
+
+
+    ;; Like-term collection
+
+    (rule `(+ (?? a)   (? b)   (?? c)   (? b)   (?? d))
+      `(+ (* 2 ,b) ,@a ,@c ,@d))
+
+    (rule `(+ (?? a)   (* (? e ,number?) (?? b))   (?? c)   (?? b)   (?? d))
+      `(+ (* (+ 1 ,e) ,@b) ,@a ,@c ,@d))
+
+    (rule `(+ (?? a)   (?? b)   (?? c)   (* (? e ,number?) (?? b))   (?? d))
+      `(+ (* (+ 1 ,e) ,@b) ,@a ,@c ,@d))
+
+    (rule `(+ (?? a)   (* (? e ,number?) (?? b))   (?? c)   (* (? f ,number?) (?? b))   (?? d))
+      `(+ (* (+ ,e ,f) ,@b) ,@a ,@c ,@d))
+
+    )))
+
+#|
+1 ]=> (algebra-3 '(+ y (* x -2 w) (* x 4 y) (* w x) z (* 5 z) (* x w) (* x y 3)))
+;Value 11: (+ y (* 6 z) (* 7 x y))
+
+1 ]=> (algebra-2 '(+ (* 4 x) (* 3 x)))
+;Value 12: (+ (* 3 x) (* 4 x))
+|#
