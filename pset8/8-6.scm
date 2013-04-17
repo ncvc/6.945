da085fe8fb2ea68fa33b9759dbc7c2dd5561d00c
Nathan V-C <ncvc@mit.edu>
2013-04-17 02:19:52 -0400

    done 8.6

--- pset8/8-6.scm
+++ pset8/8-6.scm
@@ -5,3 +5,35 @@
       (lambda ()
         (generator (pipe-writer pipe))))
     (pipe-reader pipe)))
+
+
+#|
+;;; Sample output
+
+1 ]=> (with-time-sharing-conspiracy
+      (lambda ()
+        (tf-piped-same-fringe?
+         '((a b) c ((d)) e (f ((g h))))
+         '(a b c ((d) () e) (g (f ))))
+        ))
+
+;Value: #f
+
+1 ]=> (with-time-sharing-conspiracy
+      (lambda ()
+        (tf-piped-same-fringe?
+         '((a b) c ((d)) e (f ((g h))))
+         '(a b c ((d) () e) (g (f (h)))))
+        ))
+
+;Value: #f
+
+1 ]=> (with-time-sharing-conspiracy
+      (lambda ()
+        (tf-piped-same-fringe?
+         '((a b) c ((d)) e (f ((g h))))
+         '(a b c ((d) () e) (f (g (h)))))
+        ))
+
+;Value: #t
+|#
